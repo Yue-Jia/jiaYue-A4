eafc1498810ed44dc901094dc3a1bdaa21df7e2b
Yue-Jia <52579897+Yue-Jia@users.noreply.github.com>
2019-08-08 21:29:35 -0400

    Add files via upload

---
+++ assets/myInfo.ts
@@ -0,0 +1,8 @@
+import {JiaYu} from'../app/jiaYu';
+export const JIAYU:JiaYu={
+    studentnumber:'991469751',
+    name:'Yue Jia',
+    loginname:'jiayu',
+    campus:'trafagar',
+    assigntitle:'jiaYu-A4'
+};
